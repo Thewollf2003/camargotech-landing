40589c4539d2494bc3470652ae759ba93a08de17
Thewollf2003 <173120285+Thewollf2003@users.noreply.github.com>
2026-08-08 15:28:47 -0400

    agregar robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://camargotech-landing.vercel.app/sitemap.xml',
+  }
+}
